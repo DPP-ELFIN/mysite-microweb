cd7d39a0dcf4e17566b4eb70a2f3430ee08676ea
大月月鸟 <1519037604@qq.com>
2023-09-26 16:13:29 +0800

    tinymce

--- web/mySite-react/src/App.tsx
+++ web/mySite-react/src/App.tsx
@@ -2,14 +2,9 @@
 
 import { memo, Suspense } from "react";
 import type { FC, ReactNode } from "react";
-// import AppHeader from "./commponents/app-header";
-// import AppFooter from "./commponents/app-footer";
 import "@/assets/css/app.less";
 import { useRoutes } from "react-router-dom";
 import routes from "./router";
-// import Tools from "./commponents/tools";
-import Layout from "./commponents/layout";
-import { Outlet } from "react-router-dom";
 
 /** @format */
 interface IProps {
@@ -19,15 +14,7 @@ interface IProps {
 const App: FC<IProps> = () => {
   return (
     <div className="app">
-      {/* <AppHeader />
-      <Suspense fallback="loading...">
-        <div className="main">{useRoutes(routes)}</div>
-        <Tools />
-      </Suspense>
-      <AppFooter /> */}
-      {/* <Layout /> */}
-      {/* <Outlet /> */}
-      {useRoutes(routes)}
+      <Suspense fallback={"loading..."}>{useRoutes(routes)}</Suspense>
     </div>
   );
 };
--- web/mySite-react/src/commponents/tools/index.tsx
+++ web/mySite-react/src/commponents/tools/index.tsx
@@ -4,6 +4,7 @@ import React, { memo } from "react";
 import type { FC, ReactNode } from "react";
 import { CommentOutlined, ToolOutlined } from "@ant-design/icons";
 import { FloatButton, Tooltip } from "antd";
+import { useNavigate } from "react-router-dom";
 
 /** @format */
 interface IProps {
@@ -11,11 +12,15 @@ interface IProps {
 }
 
 const Tools: FC<IProps> = () => {
+  const navigate = useNavigate();
+  const goFullEditors = () => {
+    navigate("/fullEditors");
+  };
   return (
     <div>
       <FloatButton.Group trigger="hover" type="primary" style={{ right: 94 }} icon={<ToolOutlined />}>
         <Tooltip placement="left" title={"富文本编辑器"}>
-          <FloatButton />
+          <FloatButton onClick={goFullEditors} />
         </Tooltip>
         {/* <FloatButton icon={<CommentOutlined />} /> */}
       </FloatButton.Group>
